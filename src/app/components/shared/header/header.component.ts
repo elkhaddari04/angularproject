29cbd6234bf3e1ac5eeffd5f865e451ab21e60b9
elkhaddari.ayoub4 <elkhaddari.ayoub4@gmail.com>
2021-03-01 23:25:53 +0100

    test

--- src/app/components/shared/header/header.component.ts
+++ src/app/components/shared/header/header.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { Product } from 'src/app/modals/product.model';
 import { CartItem } from 'src/app/modals/cart-item';
 import { CartService } from '../services/cart.service';
-import { SidebarMenuService } from '../sidebar/sidebar-menu.service';
 
 @Component({
   selector: 'app-header',
@@ -12,15 +11,9 @@ import { SidebarMenuService } from '../sidebar/sidebar-menu.service';
 export class HeaderComponent implements OnInit {
   public sidenavMenuItems:Array<any>;
 
-  public currencies = ['USD', 'EUR'];
+  public currencies = ['MAD ',];
   public currency:any;
-  public flags = [
-    { name:'English', image: 'assets/images/flags/gb.svg' },
-    { name:'German', image: 'assets/images/flags/de.svg' },
-    { name:'French', image: 'assets/images/flags/fr.svg' },
-    { name:'Russian', image: 'assets/images/flags/ru.svg' },
-    { name:'Turkish', image: 'assets/images/flags/tr.svg' }
-  ]
+
   public flag:any;
 
 
